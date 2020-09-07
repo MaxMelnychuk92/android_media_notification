51ea52b5c4c9e2ee8d5a8d84f1c4f420e7522f9f
Max <melnychuk.m.o@gmail.com>
2020-09-07 19:49:37 +0300

    1

---
+++ android/src/main/java/com/tarafdari/flutter_media_notification/NotificationReturnSlot.java
@@ -0,0 +1,49 @@
+package com.tarafdari.flutter_media_notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+public class NotificationReturnSlot extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        switch (intent.getAction()) {
+            case "prev":
+                FlutterMediaNotificationPlugin.callEvent("prev");
+                break;
+            case "next":
+                FlutterMediaNotificationPlugin.callEvent("next");
+                break;
+            case "retro":
+                FlutterMediaNotificationPlugin.callEvent("retro");
+                break;
+            case "forward":
+                FlutterMediaNotificationPlugin.callEvent("forward");
+                break;
+
+            case "toggle":
+                String title = intent.getStringExtra("title");
+                String author = intent.getStringExtra("author");
+                boolean play = intent.getBooleanExtra("play",true);
+
+                if(play)
+                    FlutterMediaNotificationPlugin.callEvent("play");
+                else
+                    FlutterMediaNotificationPlugin.callEvent("pause");
+
+                FlutterMediaNotificationPlugin.showNotification(title, author,play);
+                break;
+            case "select":
+                Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+                context.sendBroadcast(closeDialog);
+                String packageName = context.getPackageName();
+                PackageManager pm = context.getPackageManager();
+                Intent launchIntent = pm.getLaunchIntentForPackage(packageName);
+                context.startActivity(launchIntent);
+
+                FlutterMediaNotificationPlugin.callEvent("select");
+        }
+    }
+}
