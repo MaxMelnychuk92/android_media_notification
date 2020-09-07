d3efc96b66c8dd3b33341d84c88d7dafdd3e5762
Max <melnychuk.m.o@gmail.com>
2020-09-07 18:53:11 +0300

    Initial commit

---
+++ test/android_media_notification_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:android_media_notification/android_media_notification.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
